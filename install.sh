60db44857ac8660faea3b14482e7a0ec2ef15735
Levi Herrin <levi.8008@gmail.com>
2026-08-08 10:49:51 -0400

    make location agnostic

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Automate symlinking dotfiles on new systems
 
-ln -sf ~/repos/dotfiles/bashrc ~/.bashrc
-ln -sf ~/repos/dotfiles/bash_aliases ~/.bash_aliases
+DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+ln -sf "$DOTFILES_DIR/bashrc" ~/.bashrc
+ln -sf "$DOTFILES_DIR/bash_aliases" ~/.bash_aliases
 
 echo "Dotfiles installed! Run 'source ~/.bashrc' to reload."
